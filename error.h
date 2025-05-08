c41256b05a93419e545388bca995b542f42e0295
Kaïss Ramirez Briquez <ramirez.kaiss@icloud.com>
2025-05-08 19:51:24 +0200

    New (error.h): starting error.h with define strings

---
+++ error.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaissramirez <kaissramirez@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 19:41:28 by kaissramire       #+#    #+#             */
+/*   Updated: 2025/05/08 19:50:53 by kaissramire      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_H
+# define ERROR_H
+
+#define ERR_MALLOC "Error : Malloc failed\n"
+#define ERR_MANY_ARGS ": too many arguments\n"
+#define ERR_INV_ID ": not a valid identifier\n"
+#define ERR_CMD_NOT_FOUND ": command not found\n"
+
+
+
+#endif
