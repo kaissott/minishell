9bda499a28249afeb71fcfea1e9d1f1236a1b938
Luca D <luca.debionne@yahoo.fr>
2025-05-20 18:49:44 +0200

    Secured ft_strdup, return NULL if the string length is NULL.

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ludebion <ludebion@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 18:09:51 by ludebion          #+#    #+#             */
-/*   Updated: 2024/11/24 07:55:50 by ludebion         ###   ########.fr       */
+/*   Updated: 2025/05/20 04:28:17 by luca             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,12 @@ static char	*ft_strcpy(char *dest, const char *src)
 char	*ft_strdup(const char *s)
 {
 	char	*str;
+	size_t	len;
 
-	str = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+	len = ft_strlen(s);
+	if (!len)
+		return (NULL);
+	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	str = ft_strcpy(str, s);
