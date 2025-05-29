573077826b8e5768ec834f09e318e1885a2af425
Luca D <luca.debionne@yahoo.fr>
2025-05-29 23:38:28 +0200

    WIP: Add check_file function to open or create files when needed and return the file descriptor.

---
+++ src/utils/file_utils.c
@@ -0,0 +1,12 @@
+#include "../../includes/minishell.h"
+
+int	check_file(const char *filepath, t_token_type file_type)
+{
+	if (file_type == T_REDIR_IN)
+		return (open(filepath, O_RDONLY));
+	else if (file_type == T_REDIR_TRUNC)
+		return (open(filepath, O_CREAT | O_WRONLY | O_TRUNC, 0644));
+	else if (file_type == T_REDIR_APPEND)
+		return (open(filepath, O_CREAT | O_WRONLY | O_APPEND, 0644));
+	return (-1);
+}
